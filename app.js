a1c5714d62be91cd55f6791ff92a7f6554fd7b82
Bolokotono-Bugs <bug.bolokotono@gmail.com>
2024-06-02 22:11:25 +0700

    add DOM Toreto

--- app.js
+++ app.js
@@ -1,3 +1,56 @@
+const filmHoror = document.getElementsByTagName("article")[0];
+const filmKomedi = document.getElementsByTagName("article")[1];
+const filmAction = document.getElementsByTagName("article")[2];
+
+function articleEffect() {
+    filmKomedi.style.backgroundColor = "green";
+    filmKomedi.style.color = "red";
+    filmKomedi.style.scale = "1.5";
+    filmKomedi.style.transition = "500ms";
+
+    filmHoror.style.scale = "0.8";
+    filmHoror.style.transition = "1500ms";
+
+    filmAction.style.scale = "0.8";
+    filmAction.style.transition = "1500ms";
+}
+
+filmKomedi.addEventListener("click", articleEffect);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 function setNewElement() {
     const inputNamaLengkap = prompt("masukan username");
     namaLengkap.textContent = inputNamaLengkap;
