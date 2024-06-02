7214552b1f5fe5ae54d59eac2e0c257bbe88fef8
Bolokotono-Bugs <bug.bolokotono@gmail.com>
2024-06-02 21:41:59 +0700

    hanya projek abal abal

---
+++ app.js
@@ -0,0 +1,22 @@
+function setNewElement() {
+    const inputNamaLengkap = prompt("masukan username");
+    namaLengkap.textContent = inputNamaLengkap;
+
+    const inputFilmHoror = prompt("masukan film horor");
+    filmHoror.textContent = inputFilmHoror;
+
+    const inputDeskripsi1 = prompt("masukan deskripsi film horor");
+    deskripsi1.textContent = inputDeskripsi1;
+
+    const inputFilmKomedi = prompt("masukan film komedi");
+    filmKomedi.textContent = inputFilmKomedi;
+
+    const inputDeskripsi2 = prompt("masukan deskripsi film komedi");
+    deskripsi2.textContent = inputDeskripsi2;
+
+    const inputFilmAction = prompt("masukan film action");
+    filmAction.textContent = inputFilmAction;
+
+    const inputDeskripsi3 = prompt("masukan deskripsi film action");
+    deskripsi3.textContent = inputDeskripsi3;
+}
